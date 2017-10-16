4bf8c375182cfe82b9409896290fdef39876e236
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2017-10-16 11:22:21 +0100

    show storage counts

--- www/js/index.js
+++ www/js/index.js
@@ -11,9 +11,16 @@ var app = {
   receivedEvent: function(id) {
     var parentElement = document.getElementById(id);
 
-    alert("Device is Ready");
     window.localStorage.setItem("Key1", "Value1");
     window.localStorage.setItem("name", "gino");
     window.sessionStorage.setItem("age", "21");
+
+    $("localStorageCount").text(
+      "I currently have " + window.localStorage.length + "items in my Local storage"
+    );
+
+    $("sessionStorageCount").text(
+      "I currently have " + window.sessionStorage.length + "items in my Session storage"
+    );
   }
 }
